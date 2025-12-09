2cc01672ceca62b0fc1b36219ee931c56e5af50c
Meh2552 <pomarisjohnnathan9@gmail.com>
2025-12-09 22:23:36 +0800

    UserAuth hotfix: changed signup structure

--- FInalsProjectEstudyante/src/package1/UserAuth.java
+++ FInalsProjectEstudyante/src/package1/UserAuth.java
@@ -42,74 +42,101 @@ public class UserAuth
     {
 
         boolean isSignUp = true;
-        String user, pass = "", conPass, stNum, name;
+        String user = "", pass = "", conPass, stNum = "", name = "";
+
+
+        int step = 1;
 
         do 
         { 
+            switch (step) {
+
+                // Username
+                case 1:
+                System.out.println("=== SIGN UP ===");
+                user = system.validate().requireText("Username (Type 'x' to go back) : ");
             
-            System.out.println("=== SIGN UP ===");
-
-            user = system.validate().requireText("Username (Type 'x' to go back) : ");
-            
-            if (user.matches("[xX]")) 
-            {
-                return;
-            }
-
-            // Loop for password
-            while (isSignUp) 
-            {
-
-                pass = system.validate().requireText("Password (Type 'x' to go back) : ");
-                if (pass.matches("[xX]")) 
+                if (user.matches("[xX]")) 
                 {
-                    break;
-                }
-
-                conPass = system.validate().requireText("Confirm Password (Type 'x' to go back) : "); // Confirm password
-                if (conPass.matches("[xX]")) {
-                    continue;
+                    return;
                 }
 
                 // Confirms if username is already in the system
                 if (system.userManager().usernameExists(user)) {
                     System.out.println("Username exists.");
+                    continue;
+                }
+
+                step++;
+                break;
+
+
+                // Password
+                case 2:
+                pass = system.validate().requireText("Password (Type 'x' to go back) : ");
+                if (pass.matches("[xX]")) {
+                    step--;
+                    break;
+                }
+                step++;
+                break;
+
+
+                // Confirm Passowrd
+                case 3:
+                conPass = system.validate().requireText("Confirm Password (Type 'x' to go back) : "); // Confirm password
+                if (conPass.matches("[xX]")) {
+                    step--;
                     break;
                 }
 
                 // Confirms if password and confirmed password is the same
-                if (pass.equals(conPass)) {
-                    isSignUp = false;
-                } else {
-                    System.out.println("Incorrect password, type your password again to confirm");
+                if (pass.equals(conPass)) step++;
+                else System.out.println("Incorrect password, type your password again to confirm");
+                break;
+
+
+                // Full Name
+                case 4:
+
+                // Keeps generating random student numbers until one that isn't occupied
+                do {
+                    stNum = system.userManager().studentNumGenerate();
+                } while (system.userManager().studentNumExists(stNum));
+                // End of pass loop
+
+                name = system.validate().requireText("Full name: (X to go back) ");
+                if (name.matches("[xX]")) {
+                    step = 2;
+                    break;
+                }
+                step++;
+                break;
+
+
+                // Confirm
+                case 5:
+                System.out.println("= Confirm =");
+                System.out.println("Username: " + user);
+                System.out.println("Full name: " + name);
+                System.out.println("Generated Student Number: " + stNum);
+
+                String confirm = system.validate().editCancelContinue();
+                if (confirm.equals("EDIT")) {
+                    step = 1;
+                    continue;
+                } 
+                
+                else if (confirm.equals("CANCEL")) {
+                    return;
                 }
 
+                step++;
+                break;
             }
-
-            // Keeps generating random student numbers until one that isn't occupied
-            do {
-                stNum = system.userManager().studentNumGenerate();
-            } while (system.userManager().studentNumExists(stNum));
-            // End of pass loop
-
-            name = system.validate().requireText("Full name: ");
-
-            System.out.println("= Confirm =");
-            System.out.println("Username: " + user);
-            System.out.println("Full name: " + name);
-            System.out.println("Generated Student Number: " + stNum);
-
-            String confirm = system.validate().editCancelContinue();
-            if ( confirm.equals("EDIT"))  {
-                isSignUp = true;
-            }
-            else if (confirm.equals("CANCEL")) {
-                return;
-            }
-
         } 
         
-        while (isSignUp); // end of signup loop
+        while (step <= 5); // end of signup loop
         
         UserRecord r = new UserRecord(user, pass, stNum, name);
         system.userManager().addUser(r);
